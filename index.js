e46cee35af165db66fc97ff1089ef895d49f5158
mjschuetze102 <mjschuetze102@gmail.com>
2020-03-23 22:14:02 -0400

    Made the invisible I visible again

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ client.on('message', async msg => {
 			"\nWe are an official RIT organization. PLEASE no profanity, harrassment, sexual comments, or anything else made to mentors or other mentees" +
 			"\nIf you are overly aggressive to our mentors or break any of the rules above, you will be banned permanently and you will be reported to RIT" +
 			"\n```" +
-			"\nf you would like to mute this channel to prevent being spammed with notifications, right click on the channel in the navigation bar to the left, navigate to \"Notifications\" and select \"Only @mentions\""
+			"\nIf you would like to mute this channel to prevent being spammed with notifications, right click on the channel in the navigation bar to the left, navigate to \"Notifications\" and select \"Only @mentions\""
 		)
 	} else if (msg.content.toLowerCase().startsWith("!ping")) {
 		var online = msg.guild.roles.find(role => role.name === "Online Mentor");
