1b7cb46d2d60127c8a7605c0d4d220c653981212
Shantanav Saurav <ss9415@g.rit.edu>
2020-11-16 14:24:39 -0500

    double oops

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ client.on('message', async msg => {
 	let now = new Date();
 	if (now.getHours() < 9 || now.getHours() > 17 || now.getDay() > 5) {
 		if (msg.content.toLowerCase().startsWith("!") && !mentor) {
-			msg.channel.send("Sorry ${msg.author}, but we currently only offer online mentoring from 10AM-6PM on Monday through Friday. " +
+			msg.channel.send(`Sorry ${msg.author}, but we currently only offer online mentoring from 10AM-6PM on Monday through Friday. ` +
 				"However Monday through Thursday we offer mentoring in the SSE 10AM-6PM. " +
 				"If you have just a quick question feel free to post it in " +
 				"<#691857971675791370> and someone might be able to help!")
